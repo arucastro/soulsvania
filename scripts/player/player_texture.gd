1e8b8b265f7d2c53520d4f6b33b5a9f879409b13
sandro <sandrosilva9029@gmail.com>
2024-03-01 15:37:08 -0300

    character jumping and landing animations

--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -2,10 +2,17 @@ extends Sprite2D
 class_name PlayerTexture
 
 @export var animation: AnimationPlayer
+@export var player: CharacterBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
+	if (direction.y != 0):
+		vertical_behavior(direction)
+	elif (player.landing == true):
+		animation.play("landing")
+		player.set_physics_process(false)
+	else:
+		horizontal_behavior(direction)
 	print(direction)
 
 func verify_position(direction: Vector2) -> void: 
@@ -16,3 +23,16 @@ func verify_position(direction: Vector2) -> void:
 
 func horizontal_behavior(direction: Vector2) -> void:
 	animation.play("run") if (direction.x != 0) else animation.play("idle")
+
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.landing = true
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play("jump")
+
+func on_animation_finished(anim_name: String):
+	match anim_name:
+		"landing":
+			player.landing = false
+			player.set_physics_process(true)
