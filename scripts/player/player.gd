01a267a2defe4c4241439f8f5e8254a10a054079
sandro <sandrosilva9029@gmail.com>
2024-03-06 10:51:02 -0300

    code for attack, crouch, defend on player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -5,6 +5,11 @@ class_name Player
 
 var jump_count: int = 0
 var landing: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+
+var can_track_input: bool = true
 
 @export var speed: int = 0
 @export var jump_speed: int = 0
@@ -14,6 +19,7 @@ var landing: bool = false
 func _physics_process(delta: float):
 	horizontal_movement_env()
 	vertical_movement_env()
+	actions_env()
 	
 	gravity(delta)
 	
@@ -28,10 +34,36 @@ func horizontal_movement_env() -> void:
 func vertical_movement_env() -> void:
 	if (is_on_floor()):
 		jump_count = 0
-	if (Input.is_action_just_pressed("ui_select") and jump_count < 2):
+	if (Input.is_action_just_pressed("ui_select") && jump_count < 2):
 		jump_count += 1;
 		velocity.y = jump_speed
 
+func actions_env() -> void:
+	attack()
+	crouch()
+	defend()
+
+func attack() -> void:
+	var attack_condition: bool = !attacking && !crouching && !defending
+	if (Input.is_action_just_pressed("attack") && attack_condition && is_on_floor()):
+		attacking = true
+
+func crouch() -> void:
+	if (Input.is_action_pressed("crouch") && is_on_floor() && !defending):
+		crouching = true
+		defending = false
+		can_track_input = false
+	elif (!defending):
+		crouching = false
+		can_track_input = true
+
+func defend() -> void:
+	if (Input.is_action_pressed("defend") && is_on_floor() && !crouching):
+		defending = false
+		can_track_input = false
+	elif (!crouching):
+		defending = false
+		can_track_input = true
 
 func gravity(delta: float) -> void:
 	velocity.y += delta * player_gravity
